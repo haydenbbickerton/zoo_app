ff277ff4a0ce868f268bf6f3cb0a2aafcc8ae98f
Hayden Bickerton <2454958+haydenbbickerton@users.noreply.github.com>
2025-02-05 23:23:45 +0000

    Use Cat class to type the prop

---
+++ src/components/CatItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card :to="`/cat/${cat.id}`">
+    <v-img class="bg-grey-lighten-2" max-height="125" :src="cat.imageUrl" cover></v-img>
+    <v-card-title class="text-h6">
+      {{ cat.name }}
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+import { Cat } from "@/api/cat";
+
+export default {
+  props: {
+    cat: {
+      type: Cat,
+      required: true,
+    },
+  },
+};
+</script>
